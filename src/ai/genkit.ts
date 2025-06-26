3d4634d070a37291a93a4c5140ce114a063269bc
Hafsa Waseem <141602662+Hafsa-Waseem@users.noreply.github.com>
2025-06-26 13:39:30 +0500

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,10 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import { config } from 'dotenv';
+config();
+
+import { genkit } from 'genkit';
+import { googleAI } from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({ apiKey: 'AIzaSyDYtf3FCePDlU__T67NuZTnpAJnirXUcA0' })],
   model: 'googleai/gemini-2.0-flash',
 });
