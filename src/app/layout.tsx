a03c1027521d188103bd922cd74f4c38ea01e838
Hafsa Waseem <141602662+Hafsa-Waseem@users.noreply.github.com>
2025-06-27 12:58:44 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Roast-My-Code',
@@ -16,13 +17,18 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
+      <body className="font-body antialiased">
+        
+        {children}
+        <Toaster />
 
         {/* Google Adsense */}
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5699659351458850" crossOrigin="anonymous"></script>
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5699659351458850" crossOrigin="anonymous" />
 
         {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QB1N9YWEGY"></script>
-        <script dangerouslySetInnerHTML={{
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QB1N9YWEGY" />
+        <Script id="gtag-init" dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -31,24 +37,21 @@ export default function RootLayout({
           `,
         }} />
 
-       <script type="text/javascript">
-	atOptions = {
-		'key' : 'bc2c6afb4f6e29c71bbc0aa6f917fd57',
-		'format' : 'iframe',
-		'height' : 300,
-		'width' : 160,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="//pierconditioner.com/bc2c6afb4f6e29c71bbc0aa6f917fd57/invoke.js"></script>
+        {/* Adsterra Ads */}
+        <Script id="adsterra-init" dangerouslySetInnerHTML={{
+          __html: `
+            atOptions = {
+              'key' : 'bc2c6afb4f6e29c71bbc0aa6f917fd57',
+              'format' : 'iframe',
+              'height' : 300,
+              'width' : 160,
+              'params' : {}
+            };
+          `,
+        }} />
+        <Script async src="//pierconditioner.com/bc2c6afb4f6e29c71bbc0aa6f917fd57/invoke.js" />
+        <Script async src="//pierconditioner.com/ad/f9/99/adf99978c8b316ef8a2a33cc7f57f165.js" />
 
-        <script type='text/javascript' src='//pierconditioner.com/ad/f9/99/adf99978c8b316ef8a2a33cc7f57f165.js'></script>
-
-      </head>
-      <body className="font-body antialiased">
-        {children}
-        <Toaster />
-         <script async src="//pierconditioner.com/ad/f9/99/adf99978c8b316ef8a2a33cc7f57f165.js"></script>
       </body>
     </html>
   );
